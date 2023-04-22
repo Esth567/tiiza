7e57a029d0002055db1843f9e8e96578214677bf
callistus406 <callistus406@gmail.com>
2023-04-22 08:53:31 -0700

    modified

--- backend/api/routes/profile.js
+++ backend/api/routes/profile.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
-const { getCustomersProfile, updateCustomersProfile } = require('../controllers');
+const { getCustomersProfile, updateCustomersProfile, updateNumberCtrl } = require('../controllers');
 const VerifyUser = require('../../middleware/auth');
 
 router.get('/customer/profile', VerifyUser.ensureAuthenticated, getCustomersProfile);
+router.post('/customer/update/phone', updateNumberCtrl);
 router.patch('/customer/update/profile', VerifyUser.ensureAuthenticated, updateCustomersProfile);
 
 module.exports = router;
