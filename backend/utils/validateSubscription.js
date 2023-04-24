8a39a73eca7753a6164c7854f0e3e9a6f2425bca
callistus406 <callistus406@gmail.com>
2023-04-24 08:13:23 -0700

    added cron job

--- backend/utils/validateSubscription.js
+++ backend/utils/validateSubscription.js
@@ -1,30 +1,30 @@
-const { Sequelize } = require("sequelize");
-const SubscriptionModel = require("../models/subscriptionModel")
+const {Sequelize} = require('sequelize');
+const SubscriptionModel = require('../models/subscriptionModel');
 const cron = require('node-cron');
-const { sequelize } = require("../db/connect");
+const {sequelize} = require('../db/connect');
 
-// cron.schedule('*/60 * * * * *', async () => {
-//     console.log("job")
-//     const expiredSubscriptions = await SubscriptionModel.findAll({
-//         where: {
-//             endDate: {
-//                 [Sequelize.Op.lte]: new Date(),
-//             },
-//         },
-//     });
-//     console.log(expiredSubscriptions)
-//     for (const subscription of expiredSubscriptions) {
-//         await subscription.destroy();
-//     }
-// });
+cron.schedule('*/60 * * * * *', async () => {
+  // console.log('job');
+  const expiredSubscriptions = await SubscriptionModel.findAll({
+    where: {
+      endDate: {
+        [Sequelize.Op.lte]: new Date(),
+      },
+    },
+  });
+  // console.log(expiredSubscriptions);
+  for (const subscription of expiredSubscriptions) {
+    await subscription.destroy();
+  }
+});
 
-const checkSubscription = async (req) => {
-    const { user_id } = req.user
-    const getSubscription = await SubscriptionModel.findOne({ where: { customer_id: user_id } });
-    console.log(getSubscription)
-    if (!getSubscription) return false;
-    return true
+const checkSubscription = async req => {
+  const {user_id} = req.user;
+  const getSubscription = await SubscriptionModel.findOne({
+    where: {customer_id: user_id},
+  });
+  if (!getSubscription) return false;
+  return true;
+};
 
-}
-
-module.exports = { checkSubscription }
+module.exports = {checkSubscription};
