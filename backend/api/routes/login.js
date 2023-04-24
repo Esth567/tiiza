c61bf21bbaaa183b763134e83287838bd03765d1
callistus406 <callistus406@gmail.com>
2023-04-24 08:10:21 -0700

    added event logger

--- backend/api/routes/login.js
+++ backend/api/routes/login.js
@@ -2,77 +2,93 @@ const router = require('express').Router();
 const passport = require('passport');
 const initializePassport = require('../../services/passportConfig');
 const UserModel = require('../../models/userModel');
-const { sendSMSOtp } = require('../../utils/sendSMSOtp');
-const { createCustomError } = require('../../middleware/customError');
-const SubscriptionModel = require('../../models/subscriptionModel');
-require("dotenv").config()
-// const { verifyBvn } = require('../../utils/bvnVerification');
+const {sendSMSOtp} = require('../../utils/sendSMSOtp');
+const {createCustomError} = require('../../middleware/customError');
+const {logger} = require('../../utils/winstonLogger');
+const {errorLogger} = require('../../utils/fileLogger');
+require('dotenv').config();
 initializePassport(
   passport,
-  (email) => UserModel.findOne({ where: { email: email } }),
-  (id) => UserModel.findOne({ where: { user_id: id } })
+  email => UserModel.findOne({where: {email: email}}),
+  id => UserModel.findOne({where: {user_id: id}}),
 );
 router.post(
   '/login',
 
   (req, res, next) => {
     passport.authenticate('local', (error, user, info) => {
-      if (error) return res.status(400).json({ success: false, payload: info });
-      if (!user) return res.status(401).json({ success: false, payload: info });
+      if (error) {
+        return res
+          .status(400)
+          .json({success: false, payload: info.payload});
+      }
+
+      if (!user) {
+        console.log(info);
+        logger.warn(`${info.payload.logMsg}`, {
+          errorSource: 'Login',
+          userId: null,
+          errorType: 'authentication error',
+          action: 'login',
+          statusCode: info.payload.statusCode,
+          ip: req.clientIp,
+        });
+
+        return res
+          .status(401)
+          .json({success: false, payload: info.payload.message});
+      }
       if (user) {
-        // console.log(user);
         req.login(user, function (err) {
           console.log('err');
           if (err) {
             return next(err);
           }
-
-          // SubscriptionModel.create({
-          //   name: 'Example Subscription',
-          //   customer_id: user.user_id,
-          //   duration: 1 * 60, // in days
-          //   startDate: new Date(),
-          //   endDate: new Date(Date.now() + 0 * 60 * 1000), // 30 days from now
-          // }).then(response => {
-
-          //   // console.log(response)
-          // })
-          // return
-          console.log(user.is_verified)
           if (!user.is_verified) {
-            console.log("lll")
-            console.log(user.phone)
-
-            sendSMSOtp(process.env.TWILIO_FROM_NUMBER, user.phone, req).then(response => {
-              // req.session.customer_details = {}
-              req.session.customer_details = user;
-              return res.status(200).json({
-                success: true,
-                payload: { message: `OTP has been sent to ${user.phone}`, authUrl: '/customer/validate-otp' },
+            sendSMSOtp(
+              process.env.TWILIO_FROM_NUMBER,
+              user.phone,
+              req,
+            )
+              .then(response => {
+                req.session.customer_details = user;
+                return res.status(200).json({
+                  success: true,
+                  payload: {
+                    message: `OTP has been sent to ${user.phone}`,
+                    authUrl: '/customer/validate-otp',
+                  },
+                });
+              })
+              .catch(error => {
+                console.log(error);
+                return next(
+                  createCustomError(
+                    'System is unable to connect to your phone.please try again later',
+                    500,
+                  ),
+                );
               });
-            }).catch(error => {
-              console.log(error)
-              return next(
-                createCustomError('System is unable to connect to your phone.please try again later', 500)
-              );
-            })
+          } else {
+            return res
+              .status(200)
+              .send({success: true, payload: 'Login successful'});
           }
-          return res.status(200).send({ success: true, payload: "Login successful" })
-
-          // return res.redirect('/api/v1/customer/dashboard');
         });
       }
-
     })(req, res, next);
-  }
+  },
 );
 
 router.post('/logout', (req, res, next) => {
   req.logOut(function (error) {
     if (error) return next(error);
-    return res.redirect('/');
+    return res.redirect('/customer/dashboard');
   });
-  return res.status(200).json({ success: true, payload: 'you are successfully  logged out' });
+  return res.status(200).json({
+    success: true,
+    payload: 'you are successfully  logged out',
+  });
 });
 
 module.exports = router;
