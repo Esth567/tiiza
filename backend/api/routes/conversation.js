0ded2c9674c15d5dbbb6ba8532657db9be60aef6
callistus406 <callistus406@gmail.com>
2023-04-25 08:20:16 -0700

    formatted

--- backend/api/routes/conversation.js
+++ backend/api/routes/conversation.js
@@ -1,16 +1,33 @@
-const { verify } = require("jsonwebtoken")
-const { InitiateConversationCtrl, getConversationCtrl, sendMessageCtrl, fetchMessageCtrl } = require("../controllers")
-const VerifyUser = require("../../middleware/auth")
+const {verify} = require('jsonwebtoken');
+const {
+  InitiateConversationCtrl,
+  getConversationCtrl,
+  sendMessageCtrl,
+  fetchMessageCtrl,
+} = require('../controllers');
+const VerifyUser = require('../../middleware/auth');
 
-const router = require("express").Router()
+const router = require('express').Router();
 
+router.post(
+  '/customer/create/conversation',
+  VerifyUser.ensureAuthenticated,
+  InitiateConversationCtrl,
+);
+router.get(
+  '/customer/fetch/conversations',
+  VerifyUser.ensureAuthenticated,
+  getConversationCtrl,
+);
+router.post(
+  '/customer/send/message',
+  VerifyUser.ensureAuthenticated,
+  sendMessageCtrl,
+);
+router.get(
+  '/customer/fetch/messages/:conversationId',
+  VerifyUser.ensureAuthenticated,
+  fetchMessageCtrl,
+);
 
-
-router.post("/customer/create/conversation", VerifyUser.ensureAuthenticated, InitiateConversationCtrl)
-router.get("/customer/fetch/conversations", VerifyUser.ensureAuthenticated, getConversationCtrl)
-router.post("/customer/send/message", VerifyUser.ensureAuthenticated, sendMessageCtrl)
-router.get("/customer/fetch/messages", VerifyUser.ensureAuthenticated, fetchMessageCtrl)
-
-
-
-module.exports = router
+module.exports = router;
