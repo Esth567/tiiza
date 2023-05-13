a66822ae0adec66111d1b3e13a7f85d20ed55c28
callistus406 <callistus406@gmail.com>
2023-05-13 10:03:10 +0100

    patch on reset password 13:10:02

--- backend/api/index.js
+++ backend/api/index.js
@@ -6,17 +6,16 @@ const dashboard = require('./routes/dashboard');
 const otpAction = require('./routes/otpAction');
 const cardPayment = require('./routes/cardPayment');
 const conversation = require('./routes/conversation');
-const resetPasswordCtrl = require('./routes/resetPassword');
+const resetPassword = require('./routes/resetPassword');
 const lostAndFound = require('./routes/lostAndFound');
 
-router.use(resetPasswordCtrl);
+router.use(resetPassword);
 router.use(conversation);
 router.use(lostAndFound);
 
 router.use(cardPayment);
 router.use(otpAction);
 router.use(dashboard);
-console.log(register);
 router.use(register);
 router.use(profile);
 router.use(login);
